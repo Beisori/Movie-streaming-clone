17ad3c904dc2004b27ba1e34150fa18072793878
ToniGrondahl <toni.grondahl@outlook.com>
2023-11-02 12:22:31 +0200

    carousel

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css';
 import Nav from './Nav';
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function App() {
   return (
