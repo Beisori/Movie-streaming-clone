3b28f5dbe687ea15df13c8c2aa9993999676855b
ToniGrondahl <toni.grondahl@outlook.com>
2023-11-07 21:46:34 +0200

    karusellia

--- netflix-clone/src/pages/home.js
+++ netflix-clone/src/pages/home.js
@@ -1,5 +1,12 @@
-export function Home() {
-  return <h1>Home</h1>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from './Carousel';
+
+function Slider() {
+  return (
+    <div className='App'>
+      <Carousel />
+    </div>
+  )
 }
 
-export default Home;
+export default Slider
