d3a2e2f6bc868a67c200fde8625f7548e1953f63
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-09 20:41:09 +0200

    Created MovieGenres.js

---
+++ netflix-clone/src/MovieGenres.js
@@ -0,0 +1,19 @@
+import axios from "./axios"
+import React, { useEffect, useState } from 'react'
+
+export default function MovieGenres(){
+    const [genre, setGenre] = useState([]);
+
+    //create GET request for movie listing -> IDs
+    //async
+    //useState + useEffect
+
+    //Create element to return that shows movies in a grid
+    //GridContainer + individual items?
+
+    return(
+        <>
+            
+        </>
+    );
+}
