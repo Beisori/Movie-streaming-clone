a9f56d346b8aa7173e6819c3aae2ac2d44276e8c
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-09 13:33:15 +0200

    Update home.js

--- netflix-clone/src/pages/home.js
+++ netflix-clone/src/pages/home.js
@@ -8,12 +8,12 @@ export function Home() {
   <div>
     <Slider />
     <MovieSlider title="Trending" requestUrl={requests.fetchTrending}/>
-      <MovieSlider title="Top Rated" requestUrl={requests.fetchTopRated}/>
-      <MovieSlider title="Popular" requestUrl={requests.fetchPopular}/>
-      <MovieSlider title="TV Shows" requestUrl={requests.fetchTVShows}/>
-      <MovieSlider title="Action" requestUrl={requests.fetchAction}/>
-      <MovieSlider title="Comedies" requestUrl={requests.fetchComedy}/>
-      <MovieSlider title="Horror" requestUrl={requests.fetchHorror}/>
-      <MovieSlider title="Documentaries" requestUrl={requests.fetchDocumentaries}/>
+    <MovieSlider title="Top Rated" requestUrl={requests.fetchTopRated}/>
+    <MovieSlider title="Popular" requestUrl={requests.fetchPopular}/>
+    <MovieSlider title="TV Shows" requestUrl={requests.fetchTVShows}/>
+    <MovieSlider title="Action" requestUrl={requests.fetchAction}/>
+    <MovieSlider title="Comedies" requestUrl={requests.fetchComedy}/>
+    <MovieSlider title="Horror" requestUrl={requests.fetchHorror}/>
+    <MovieSlider title="Documentaries" requestUrl={requests.fetchDocumentaries}/>
   </div>
 }
