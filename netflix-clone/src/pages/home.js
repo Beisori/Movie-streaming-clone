b9c89a04b4ac803d88d8432df22dec0749e48838
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-09 13:32:51 +0200

    MovieSlider component changed to Home.js

--- netflix-clone/src/pages/home.js
+++ netflix-clone/src/pages/home.js
@@ -1,9 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Slider from '../Carousel';
+import MovieSlider from '../Movieslider';
+import requests from '../requests';
 
 export default Home;
 export function Home() {
   <div>
     <Slider />
+    <MovieSlider title="Trending" requestUrl={requests.fetchTrending}/>
+      <MovieSlider title="Top Rated" requestUrl={requests.fetchTopRated}/>
+      <MovieSlider title="Popular" requestUrl={requests.fetchPopular}/>
+      <MovieSlider title="TV Shows" requestUrl={requests.fetchTVShows}/>
+      <MovieSlider title="Action" requestUrl={requests.fetchAction}/>
+      <MovieSlider title="Comedies" requestUrl={requests.fetchComedy}/>
+      <MovieSlider title="Horror" requestUrl={requests.fetchHorror}/>
+      <MovieSlider title="Documentaries" requestUrl={requests.fetchDocumentaries}/>
   </div>
 }
