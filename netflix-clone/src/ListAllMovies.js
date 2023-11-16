d154e41b4d472c84c275d50c004bd6246e6c84eb
otto288 <ottokroger28@gmail.com>
2023-11-16 12:26:01 +0200

    Update ListAllMovies.js

--- netflix-clone/src/ListAllMovies.js
+++ netflix-clone/src/ListAllMovies.js
@@ -2,7 +2,7 @@ import { Await, useParams } from "react-router-dom"
 import axios from "axios"
 import React, { useEffect, useState }from "react";
 
-const Main = () => {
+const Allmovies = () => {
   
   const [movies, setMovies] = useState([]);
  
@@ -15,12 +15,12 @@ const Main = () => {
 const getMovies = async (id) => {
   const response = await axios.get("https://api.themoviedb.org/3/genre/movie/list${id}?api_key=eaa4d9ee470345a99f952f889c06479c&language=en")}
   const data = response.data;
+  setMovies(response.data.results)
   console.log(data);
 
   useParams: [API_KEY = "eaa4d9ee470345a99f952f889c06479c"]
 
 const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
-useEffect ({getMovies})
-
 }
+export default Allmovies;
