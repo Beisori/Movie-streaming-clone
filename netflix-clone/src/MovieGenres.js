f168bffe64fc041bc48ba0165709bab5a9304e2c
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-17 19:07:26 +0200

    Update MovieGenres.js

--- netflix-clone/src/MovieGenres.js
+++ netflix-clone/src/MovieGenres.js
@@ -4,23 +4,23 @@ import React, { useEffect, useState } from 'react'
 export default function MovieGenres(){
     const [genre, setGenre] = useState([]);
 
-    //create GET request for movie listing -> IDs
-
+    //https://api.themoviedb.org/3/genre/movie/list?api_key=eaa4d9ee470345a99f952f889c06479c&language=en
+    //^From here you can get the genre IDs for new requests.js paths
     useEffect(() => {
         async function getAllMovies() { //Add URL for request
             const response = await axios.get(requestUrl);
-            console.log('Response', response)
+            console.log('Response', response) //This doesn't work, use same type of search from discovery as in requests.js file
             setGenre(response.data.results);
         };
         getAllMovies();
-    }, [requestUrl]); //requestUrl not needed probably, empty [] for rendering on need basis
+    }, []);
 
     //Create element to return that shows movies in a flexbox with wrap
     //poster picture from API
 
     //Buttons that list movie genres based on ID
     //Individual item takes what it needs genre state
-    //Map through genre and list that genre on individual links
+    //Map through genre based on ID and list that genre on individual links (router?)
 
     return(
         <div className="genre-dropdown-container">
