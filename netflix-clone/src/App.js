ddfe3bc830dad25a088e204e7b46f6671e6f88f8
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-20 21:14:29 +0200

    Formatting updates app.js and requests.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import MovieSlider from './Movieslider';
-import requests from './requests';
-
 
 function App() {
   return (
@@ -12,17 +9,7 @@ function App() {
     <div className="App">
       {/*<Route path="home" component={home} />*/}
       <Navbar />
-
-      <MovieSlider title="Trending" requestUrl={requests.fetchTrending}/>
-      <MovieSlider title="Top Rated" requestUrl={requests.fetchTopRated}/>
-      <MovieSlider title="Popular" requestUrl={requests.fetchPopular}/>
-      <MovieSlider title="TV Shows" requestUrl={requests.fetchTVShows}/>
-      <MovieSlider title="Action" requestUrl={requests.fetchAction}/>
-      <MovieSlider title="Comedies" requestUrl={requests.fetchComedy}/>
-      <MovieSlider title="Horror" requestUrl={requests.fetchHorror}/>
-      <MovieSlider title="Documentaries" requestUrl={requests.fetchDocumentaries}/>
     </div>
-
   );
 }
 
