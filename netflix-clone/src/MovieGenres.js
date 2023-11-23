a01685c7aea2d6f215dc0e1957ae3210577bed8d
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-23 16:43:42 +0200

    Update MovieGenres.js

--- netflix-clone/src/MovieGenres.js
+++ netflix-clone/src/MovieGenres.js
@@ -7,9 +7,6 @@ const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 export default function MovieGenres({requestUrl}){
     const [movies, setMovies] = useState([]);
 
-    const [selectedValue, setSelectedValue] = useState('');
-    const [selectedOption, setSelectedOption] = useState('');
-    const [displayedInfo, setDisplayedInfo] = useState('');
 
     //https://api.themoviedb.org/3/genre/movie/list?api_key=eaa4d9ee470345a99f952f889c06479c&language=en
     //^From here you can get the genre IDs for new requests.js paths
@@ -22,29 +19,15 @@ export default function MovieGenres({requestUrl}){
         getAllMovies();
     }, [requestUrl]);
 
-    const inputChanged = (Event) => {
-        setSelectedValue(Event.target.value);
-    }
 
-    if(selectedValue === 'Action'){
-        setDisplayedInfo(requests.fetchAction);
-    }
-    setSelectedValue(selectedValue);
-    setSelectedOption(selectedValue);
 
-    //For css use flexbox-wrap
     return(
         <div className="dropdown-container">
-            <select className="dropdown-options" value={selectedValue} onChange={inputChanged}>
+            <select className="dropdown-options" value={''} onChange={''}>
                 <option value=''>select a genre</option>
                 <option value="Action">Action</option>
             </select>
 
-            {setDisplayedInfo && movies.map(movie => (
-                <div key={movie.id}>
-                    <img className="movie-image" src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.title}/>
-                </div>
-            ))}
         </div>
     );
 }
