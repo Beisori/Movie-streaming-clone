0e69aa3a55b3034f06ce442939c2ca6d06bfe7c8
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-24 15:16:08 +0200

    Update MovieGenres.js

--- netflix-clone/src/MovieGenres.js
+++ netflix-clone/src/MovieGenres.js
@@ -1,46 +1,40 @@
 import axios from "./axios"
 import React, { useEffect, useState } from 'react'
-import requests from "./requests";
+import requests from './requests';
 
+const API_KEY = "eaa4d9ee470345a99f952f889c06479c";
 const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
 export default function MovieGenres({requestUrl}){
     const [movies, setMovies] = useState([]);
-    const [genre, setGenre] = useState('');
+    const [selectedGenre, setSelectedGenre] = useState('');
 
     //https://api.themoviedb.org/3/genre/movie/list?api_key=eaa4d9ee470345a99f952f889c06479c&language=en
     //^From here you can get the genre IDs for new requests.js paths
+    
     useEffect(() => {
-        async function getAllMovies() {
-            const response = await axios.get(requestUrl);
-            console.log('Response', response)
+        async function getMoviesByGenre() {
+            const response = await axios.get(requestUrl + `discover/movie?api_key=${API_KEY}&with_genres=${selectedGenre}`);
+            console.log('Response', response) //request path doesn't work 404
             setMovies(response.data.results);
         };
-        getAllMovies();
-    }, [requestUrl]);
+        getMoviesByGenre();
+    }, [selectedGenre]);
 
-    function renderGenre(){
-        if(genre === 'Action'){
-            //Render action movies in a flex box wrap
-        }
-        //Inside this use conditional rendering, which has request inside it
-        //Choose which to render based on user input <- this information comes from inputChanged event
-    }
-
-    const inputChanged = (Event) => {
-        setGenre({...genre,[Event.target.name]: Event.target.value})
+    const genreChanged = (Event) => {
+        setSelectedGenre(Event.target.value);
     }
 
     return(
-        <div className="dropdown-container">
-            <select className="dropdown-options" value={genre} onChange={inputChanged}>
-                <option value=''>select a genre</option>
-                <option value="Action">Action</option>
+        <div className="genre-dropdown-container">
+            <h2 className="genre-title">Movies by genres</h2>
+            <select id="genreDropdown" value={selectedGenre} onChange={genreChanged}>
+                <option value="">Genres</option>
+                <option value="28">Action</option>
             </select>
 
-            <div className="movie-container">
-                <h2>Movies</h2>
-                {renderGenre && movies.map(movie => (
+            <div className="genre-movie-container">
+                {movies.map(movie => (
                     <div key={movie.id}>
                         <img className="movie-image" src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.title}/>
                     </div>
