2f63e5105e8ab5cf52d6538fb6072d040343a92c
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-24 15:30:53 +0200

    Created moviegenres.css file

--- netflix-clone/src/MovieGenres.js
+++ netflix-clone/src/MovieGenres.js
@@ -1,12 +1,12 @@
-import axios from "./axios"
-import React, { useEffect, useState } from 'react'
-import requests from './requests';
+import axios from "./axios";
+import React, { useEffect, useState } from 'react';
+import './MovieGenres.css';
 
 const API_KEY = "eaa4d9ee470345a99f952f889c06479c";
 const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
 export default function MovieGenres({requestUrl}){
-    const [movies, setMovies] = useState([]);
+    const [moviesByGenres, setMoviesByGenres] = useState([]);
     const [selectedGenre, setSelectedGenre] = useState('');
 
     //https://api.themoviedb.org/3/genre/movie/list?api_key=eaa4d9ee470345a99f952f889c06479c&language=en
@@ -14,9 +14,9 @@ export default function MovieGenres({requestUrl}){
     
     useEffect(() => {
         async function getMoviesByGenre() {
-            const response = await axios.get(requestUrl + `discover/movie?api_key=${API_KEY}&with_genres=${selectedGenre}`);
-            console.log('Response', response) //request path doesn't work 404
-            setMovies(response.data.results);
+            const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${selectedGenre}`);
+            console.log('Response', response)
+            setMoviesByGenres(response.data.results);
         };
         getMoviesByGenre();
     }, [selectedGenre]);
@@ -31,12 +31,15 @@ export default function MovieGenres({requestUrl}){
             <select id="genreDropdown" value={selectedGenre} onChange={genreChanged}>
                 <option value="">Genres</option>
                 <option value="28">Action</option>
+                <option value="35">Comedy</option>
+                <option value="99">Documentaries</option>
+                <option value="27">Horror</option>
             </select>
 
             <div className="genre-movie-container">
-                {movies.map(movie => (
+                {moviesByGenres.map(movie => (
                     <div key={movie.id}>
-                        <img className="movie-image" src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.title}/>
+                        <img className="movie-image-genres" src={`${imageBaseUrl}${movie.poster_path}`} alt={movie.title}/>
                     </div>
                 ))}
             </div>
