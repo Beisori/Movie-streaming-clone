be05ac4d36d9d9b21016af95b2d21bb28d7db5a2
samikorhonen95 <samikorhonen95@gmail.com>
2023-11-28 12:56:27 +0200

    Update App.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -4,10 +4,7 @@ import Navbar from './Navbar';
 
 function App() {
   return (
-    //implement routing with the router component
-    //change Movieslider component to home.js file
     <div className="App">
-      {/*<Route path="home" component={home} />*/}
       <Navbar />
     </div>
   );
